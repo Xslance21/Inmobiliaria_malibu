cf3923de86797a8d9497c755bcfb1a0a100f0428
Safirkz <safirpakipaisa@gmail.com>
2022-11-29 21:13:07 -0500

    Crear Footer

---
+++ footer.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style_footer.css">
+    <title>Document</title>
+</head>
+<body>
+    <footer class="footer">
+        <div class="container">
+            <div class="row d-flex text-center">
+                <div class="col">
+                    <h4>Contactenos</h4>
+                </div>
+            </div>
+            <div class="row d-inline-flex">
+                <div class="col">
+                    <ul>Numeros de contacto:
+                        <li>320 4568662</li>
+                        <li>301 3527871</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row d-inline-flex">
+                <div class="col">
+                    <ul>Correos de contacto:
+                        <li>cobromalibu@inmobili.com</li>
+                        <li>reclamomalibu@inmobili.com</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row d-flex">
+                <div class="col">
+                    <p class="description_">
+                        Somos una inmobiliaria, especializada en casa y apartementos de lujo, aunque tambien puede encontrar terrenos en la playa y las altas montañas
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
